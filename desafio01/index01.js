677599caada50c6273b88b17d6b002a41e19c3dc
João Pedro Acordi <88681190+JoaoAcordi@users.noreply.github.com>
2025-09-17 19:07:08 -0300

    abstract_factory

--- desafio01/index01.js
+++ desafio01/index01.js
@@ -1,1 +1,68 @@
-console.log("Hello, World! 001");
+class LightButton {
+  render() {
+    return "Botão branco criado";
+  }
+}
+
+class DarkButton {
+  render() {
+    return "Botão preto criado";
+  }
+}
+
+class LightWindow {
+  render() {
+    return "Janela clara aberta";
+  }
+}
+
+class DarkWindow {
+  render() {
+    return "Janela escura aberta";
+  }
+}
+
+class UIFactory {
+  createButton() {
+    throw new Error("Método createButton() deve ser implementado.");
+  }
+
+  createWindow() {
+    throw new Error("Método createWindow() deve ser implementado.");
+  }
+}
+
+class LightUIFactory extends UIFactory {
+  createButton() {
+    return new LightButton();
+  }
+
+  createWindow() {
+    return new LightWindow();
+  }
+}
+
+class DarkUIFactory extends UIFactory {
+  createButton() {
+    return new DarkButton();
+  }
+
+  createWindow() {
+    return new DarkWindow();
+  }
+}
+
+const theme = "dark";
+
+let factory;
+if (theme === "light") {
+  factory = new LightUIFactory();
+} else {
+  factory = new DarkUIFactory();
+}
+
+const button = factory.createButton();
+const windowUI = factory.createWindow();
+
+console.log(button.render());
+console.log(windowUI.render());
